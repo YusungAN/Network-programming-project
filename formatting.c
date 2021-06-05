1772a6d913909b5792a709f716ed3c402faf3df4
arcde40 <arcde40@gmail.com>
2021-06-06 00:39:40 +0900

    Implemented Client Protocol Formatter

--- formatting.c
+++ formatting.c
@@ -1,14 +1,34 @@
 #include <string.h>
 #include <stdlib.h>
+#include <string.h>
 
 // Use this function when your client have to parse Chatting Manger Server information4
 // *data: received Server information / *opcode, *port, *ip: Variable to save opcode/port/ip information
-void parseCMInfo(char *data, int *opcode, int *port, char *ip) {
-    char opcodeTemp[5] = {0,};
-    char portTemp[5] = {0,};
+void parseCMInfo(char *data, int *opcode, int *port, char *ip)
+{
+    char opcodeTemp[5] = {
+        0,
+    };
+    char portTemp[5] = {
+        0,
+    };
     strncpy(opcodeTemp, data, 4);
-    strncpy(portTemp, data+4, 4);
+    strncpy(portTemp, data + 4, 4);
     strcpy(ip, &data[8]);
     *opcode = atoi(opcodeTemp);
     *port = atoi(portTemp);
+}
+
+// Generating Message Request Protocol String - 0001LENGDESTMessage...
+// dest: Recipient Client sockfd / *str: message / *buf: target buffer
+void msgRequest(int dest, char *str, char *buf)
+{
+    sprintf(buf, "0001%04ld%04d%s", strlen(str) + 4, dest, str);
+}
+
+// Generating Initial Handshake Protocol String - 0000LENGDESTNickname...
+// *str: nickname  / *buf: target buffer
+void initialHandshakeRequest(char *str, char *buf)
+{
+    sprintf(buf, "0000%04ld%s", strlen(str), str);
 }
--- formatting.h
+++ formatting.h
@@ -1,3 +1,5 @@
 #include "formatting.c"
 
 void parseCMInfo(char *data, int *opcode, int *port, char *ip);
+void msgRequest(int dest, char *str, char *buf);
+void initialHandshakeRequest(char *str, char *buf);
