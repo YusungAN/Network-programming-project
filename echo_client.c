0fc5da426c21a2f59b4cb730dd628a4a29cb793e
YusungAN <52990109+YusungAN@users.noreply.github.com>
2021-06-11 10:08:48 +0900

    add multicast function

--- echo_client.c
+++ echo_client.c
@@ -4,34 +4,100 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
+#include <sys/types.h>
+#include "protocol.h"
+#include "formatting.h"
 
 #define BUF_SIZE 1024
 
-typedef struct
-{
-	int opcode;
-	int length;
-	char data[1024];
-} request;
+void printonlineusers(userdata *arr);
+int fetchUser(userdata *data, int sock);
+void storeList(char (*store)[1024], char *str);
+void printList(char (*store)[1024]);
 
-int main(int argc, char *argv[])
+int main()
 {
 	int sock;
+	int dest;
 	char message[BUF_SIZE];
+	char temp[BUF_SIZE];
 	int str_len;
+	int sender;
 	struct sockaddr_in serv_adr;
+	char store[30][1024] = {
+		0,
+	};
 
-	if (argc != 3)
-	{
-		printf("Usage : %s <IP> <port>\n", argv[0]);
-		exit(1);
-	}
+	fd_set readfds, allfds;
+	int fd_num;
+	respond res;
+	userdata u_data[30];
+	memset(u_data, 0, sizeof(u_data));
+	char aBuffer[5];
+
+	int mt_sock;
+	struct ip_mreq mt_join_adr;
+    struct sockaddr_in mt_from_addr;
+    struct sockaddr_in mt_serv_addr;
+    char mt_ip[20];
+    strcpy(mt_ip, "239.0.110.1");
+	int mt_port = 5100;
+	
+	printf("Finding Server...\n");
+
+	mt_sock=socket(PF_INET, SOCK_DGRAM, 0);
+	if(mt_sock == -1)
+		printf("socket() error");
+
+    memset(&mt_serv_addr, 0, sizeof(mt_serv_addr));
+	mt_serv_addr.sin_family=AF_INET;
+	mt_serv_addr.sin_addr.s_addr=inet_addr(mt_ip);
+	mt_serv_addr.sin_port=htons(mt_port);
+
+    if (bind(mt_sock, (struct sockaddr *)&mt_serv_addr, sizeof(mt_serv_addr)) == -1) {
+        printf("bind err");
+        close(mt_sock);
+        exit(1);
+    }
+
+    mt_join_adr.imr_multiaddr.s_addr = inet_addr(mt_ip);
+    mt_join_adr.imr_interface.s_addr = htonl(INADDR_ANY);
+
+    setsockopt(mt_sock, IPPROTO_IP, IP_ADD_MEMBERSHIP, (void*)&mt_join_adr, sizeof(mt_join_adr));
+	
+    int mt_str_len;
+
+    char mt_buf[1024];
+    memset(&mt_from_addr, 0, sizeof(mt_from_addr));
+    int adr_sz;
+
+    int opcode_from_s;
+    int port_from_s;
+    char ip_from_s[20];
+
+    while (1) {
+        memset(&mt_buf, 0, sizeof(mt_buf));
+        adr_sz = sizeof(mt_from_addr);
+        mt_str_len = recvfrom(mt_sock, mt_buf, sizeof(mt_buf), 0, (struct sockaddr*)&mt_from_addr, &adr_sz);
+        mt_buf[mt_str_len] = 0;
+        parseCMInfo(mt_buf, &opcode_from_s, &port_from_s, ip_from_s);
+        if (opcode_from_s == 1000) {
+			printf("Server Information: %d %d %s\n", opcode_from_s, port_from_s, ip_from_s);
+			break;
+		} else {
+			printf("Finding Server...\n");
+		}
+    }
+
+    close(mt_sock);
 
 	char nickname[30];
-
+	char bBuffer[50] = {
+		0,
+	};
 	printf("Input Nickname: ");
 	scanf("%s", nickname);
-
+	sprintf(bBuffer, "\x1b[95m[ Me ] %s : ", nickname);
 	sock = socket(PF_INET, SOCK_STREAM, 0);
 	if (sock == -1)
 	{
@@ -39,10 +105,11 @@ int main(int argc, char *argv[])
 		exit(0);
 	}
 
+	//printf("%d", strcmp(ip_from_s, "192.168.100.92"));
 	memset(&serv_adr, 0, sizeof(serv_adr));
 	serv_adr.sin_family = AF_INET;
-	serv_adr.sin_addr.s_addr = inet_addr(argv[1]);
-	serv_adr.sin_port = htons(atoi(argv[2]));
+	serv_adr.sin_addr.s_addr = inet_addr(ip_from_s);
+	serv_adr.sin_port = htons(port_from_s);
 
 	if (connect(sock, (struct sockaddr *)&serv_adr, sizeof(serv_adr)) == -1)
 	{
@@ -52,23 +119,155 @@ int main(int argc, char *argv[])
 	else
 		printf("Connected...........\n");
 
-	sprintf(message, "0000%04ld%s", strlen(nickname), nickname);
-	printf("%s\n", message);
+	initialHandshakeRequest(nickname, message);
 	write(sock, message, strlen(message));
+
+	FD_ZERO(&readfds);
+	FD_SET(STDIN_FILENO, &readfds);
+	FD_SET(sock, &readfds);
+	printf("\x1b[2J\x1b[s\x1b[B");
 	while (1)
 	{
-		memset(message, 0, BUF_SIZE);
-		fputs("Input message(Q to quit): ", stdout);
-		fgets(message, BUF_SIZE, stdin);
 
-		if (!strcmp(message, "q\n") || !strcmp(message, "Q\n"))
-			break;
+		allfds = readfds;
 
-		write(sock, message, strlen(message));
-		str_len = read(sock, message, BUF_SIZE);
-		printf("Message from server: %s", message);
+		fd_num = select(4, &allfds, (fd_set *)0, (fd_set *)0, NULL);
+
+		if (fd_num < 0)
+		{
+			printf("select() error");
+			exit(0);
+		}
+
+		if (FD_ISSET(STDIN_FILENO, &allfds))
+		{
+			memset(message, 0, BUF_SIZE);
+			//fgets(message, BUF_SIZE, stdin);
+
+			if (read(0, message, 1) < 0)
+				continue;
+			dest = atoi(message);
+			str_len = read(0, message, BUF_SIZE);
+			memset(temp, 0, 1024);
+			memcpy(temp, bBuffer, sizeof(bBuffer));
+			storeList(store, strcat(temp, message));
+			printList(store);
+			printonlineusers(u_data);
+			if (!strcmp(message, "q\n") || !strcmp(message, "Q\n"))
+				break;
+
+			msgRequest(dest, message, temp);
+			write(sock, temp, strlen(temp));
+			printf("Input Message: \n");
+		}
+
+		if (FD_ISSET(sock, &allfds))
+		{
+			memset(message, 0, sizeof(message));
+			memset(temp, 0, sizeof(temp));
+			if ((str_len = read(sock, message, BUF_SIZE)) > 0)
+			{
+				memset(&res, 0, sizeof(res));
+				memset(aBuffer, 0, 5);
+				memcpy(aBuffer, message, 4);
+				parseOpcode(aBuffer, &res.opcode);
+				memcpy(aBuffer, &message[4], 4);
+				res.length = atoi(aBuffer);
+				memcpy(res.data, &message[8], res.length);
+
+				switch (res.opcode)
+				{
+
+				case 0x8:
+					// Server Message Relay
+					/*
+                            Data - ~ 1012 byte
+                            Opcode(4) Length(4) Data( Sender(4) Data(1012) )
+                    */
+					parseChatRelay(message, &sender, temp);
+					memset(message, 0, sizeof(message));
+					sprintf(message, "\x1b[94m[%d] %s : %s", sender, u_data[fetchUser(u_data, sender)].nick, temp);
+					storeList(store, message);
+					printList(store);
+					printonlineusers(u_data);
+					printf("Input Message: \n");
+					break;
+				case 0x9:
+					parseUserList(res.data, u_data);
+					//printf("\n User data Retrieved \n");
+					printonlineusers(u_data);
+					break;
+				case 0xF:
+					// Server Error ACK
+					/*
+							Errno - 4 byte Message - ~ 1012 bytes
+							Opcode(4) Length(4) Data( Errno(4) Data(1012) )
+					*/
+
+					// TODO
+
+					break;
+				default:
+					break;
+				}
+			}
+			else
+			{
+				printf("Connection reset\n");
+				close(sock);
+				exit(1);
+			}
+		}
 	}
 
 	close(sock);
 	return 0;
+}
+
+void printonlineusers(userdata *arr)
+{
+	printf("\x1b[H\x1b[0m\x1b[K\n\x1b[KCurrently Online:\n\x1b[K----------------------\n");
+	for (int i = 0; i < 30; i++)
+	{
+		if (arr[i].connected == 2)
+		{
+			printf("\x1b[K[%d] %s \x1b[32m●  Online\x1b[0m\n", arr[i].sockfd, arr[i].nick);
+		}
+		else if (arr[i].connected == 1)
+		{
+			printf("\x1b[K[-] %s \x1b[31m●  Offline\x1b[0m\n", arr[i].nick);
+		}
+	}
+	printf("\x1b[K----------------------\n\x1b[u\x1b[B");
+}
+
+int fetchUser(userdata *data, int sock)
+{
+	for (int i = 0; i < 30; i++)
+	{
+		if (data[i].sockfd == sock && data[i].connected == 2)
+			return i;
+	}
+	return -1;
+}
+
+void storeList(char (*store)[1024], char *str)
+{
+	if (!strcmp("\n", str))
+		return;
+	for (int i = 29; i > 0; i--)
+	{
+		memcpy(&store[i], &store[i - 1], 1024);
+	}
+	memcpy(store, str, 1024);
+}
+
+void printList(char (*store)[1024])
+{
+	printf("\x1b[2J\x1b[s");
+	for (int i = 29; i >= 0; i--)
+	{
+		if (store[i] != 0)
+			printf("%s", store[i]);
+	}
 }
