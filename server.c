c8c275b15ea5a45915ef1d4aa584614ab1086f5c
YusungAN <52990109+YusungAN@users.noreply.github.com>
2021-06-14 17:32:09 +0900

    login function

--- server.c
+++ server.c
@@ -20,7 +20,11 @@
 #define PORTNUM 9100
 #define SOCK_SETSIZE 1021
 
+#define MAX_CLIENT 30
+#define MAX_NICKNAME_LENGTH 30
+
 void nonblock(int sockfd);
+void printonlineusers(userdata *arr);
 void itoa(int i, char *st);
 
 int main(int argc, char **argv)
@@ -32,8 +36,30 @@ int main(int argc, char **argv)
     int sockfd;
     int readn;
     int i = 0;
+    int on;
+
+    UserLogin loginArr[7];
+    strcpy(loginArr[0].nickname, "yusung");
+    strcpy(loginArr[0].password, "0510");
+    strcpy(loginArr[1].nickname, "yubin");
+    strcpy(loginArr[1].password, "0119");
+    strcpy(loginArr[2].nickname, "hojin");
+    strcpy(loginArr[2].password, "0906");
+    strcpy(loginArr[3].nickname, "taehwan");
+    strcpy(loginArr[3].password, "1006");
+    strcpy(loginArr[4].nickname, "seongjip");
+    strcpy(loginArr[4].password, "0510");
+    strcpy(loginArr[5].nickname, "youngmin");
+    strcpy(loginArr[5].password, "0318");
+    strcpy(loginArr[6].nickname, "soonjae");
+    strcpy(loginArr[6].password, "0322");
+
+    userdata u_data[MAX_CLIENT];
+
+    char aBuffer[5];
     char buf[MAXLINE];
     fd_set readfds, allfds;
+    request req;
 
     struct sockaddr_in server_addr, client_addr;
 
@@ -44,20 +70,20 @@ int main(int argc, char **argv)
     int multicast_state;
     char multicast_ip[20];
     strcpy(multicast_ip, "239.0.110.1");
-	int multicast_port = 5100;
+    int multicast_port = 5100;
 
-    multicast_sock=socket(PF_INET, SOCK_DGRAM, 0);
-	if(multicast_sock == -1)
-		printf("socket() error");
+    multicast_sock = socket(PF_INET, SOCK_DGRAM, 0);
+    if (multicast_sock == -1)
+        printf("socket() error");
 
     struct sockaddr_in multicast_serv_addr;
 
     memset(&multicast_serv_addr, 0, sizeof(multicast_serv_addr));
-	multicast_serv_addr.sin_family=AF_INET;
-	multicast_serv_addr.sin_addr.s_addr=inet_addr(multicast_ip);
-	multicast_serv_addr.sin_port=htons(multicast_port);
+    multicast_serv_addr.sin_family = AF_INET;
+    multicast_serv_addr.sin_addr.s_addr = inet_addr(multicast_ip);
+    multicast_serv_addr.sin_port = htons(multicast_port);
 
-    setsockopt(multicast_sock, IPPROTO_IP, IP_MULTICAST_TTL, (void*)&ttl, sizeof(ttl));
+    setsockopt(multicast_sock, IPPROTO_IP, IP_MULTICAST_TTL, (void *)&ttl, sizeof(ttl));
     //---------
 
     if ((listen_fd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
@@ -81,53 +107,58 @@ int main(int argc, char **argv)
         return 1;
     }
 
+    on = 1;
+    setsockopt(listen_fd, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on));
+
     FD_ZERO(&readfds);
     FD_SET(listen_fd, &readfds);
 
     maxfd = listen_fd;
-
     //make CM info message
     send_CM_info send_msg;
     send_msg.opcode = 1000;
-    send_msg.port = 5100;
+    send_msg.port = 9100;
     strcpy(send_msg.ip, "192.168.100.92");
 
     char CM_info_message[1024];
 
-    char opcode[4] = {0,};
-    char port[4] = {0,};
+    char opcode[4] = {
+        0,
+    };
+    char port[4] = {
+        0,
+    };
     int off = 0;
 
     memset(&CM_info_message, 0, sizeof(CM_info_message));
-	// opcode
+    // opcode
     itoa(send_msg.opcode, opcode);
-	memcpy(&CM_info_message[off], &opcode, sizeof(opcode));
-	off = sizeof(opcode);
+    memcpy(&CM_info_message[off], &opcode, sizeof(opcode));
+    off = sizeof(opcode);
 
     // port
-	itoa(send_msg.port, port);
-	memcpy(&CM_info_message[off], &port, sizeof(port));
-	off += sizeof(port);
+    itoa(send_msg.port, port);
+    memcpy(&CM_info_message[off], &port, sizeof(port));
+    off += sizeof(port);
 
     // ip
-	memcpy(&CM_info_message[off], &send_msg.ip, sizeof(send_msg.ip));
-	off += sizeof(send_msg.ip);
-
+    memcpy(&CM_info_message[off], &send_msg.ip, sizeof(send_msg.ip));
+    off += sizeof(send_msg.ip);
 
     while (1)
     {
         //multicast server info
         multicast_tv.tv_sec = 2;
         multicast_tv.tv_usec = 0;
-
         allfds = readfds;
         fd_num = select(maxfd + 1, &allfds, (fd_set *)0,
                         (fd_set *)0, &multicast_tv);
 
-        if (fd_num == 0) {
+        if (fd_num == 0)
+        {
             printf("send server info\n");
             printf("%s\n", &CM_info_message[0]);
-            sendto(multicast_sock, CM_info_message, strlen(CM_info_message), 0, (struct sockaddr*)&multicast_serv_addr, sizeof(multicast_serv_addr));
+            sendto(multicast_sock, CM_info_message, strlen(CM_info_message), 0, (struct sockaddr *)&multicast_serv_addr, sizeof(multicast_serv_addr));
         }
 
         if (FD_ISSET(listen_fd, &allfds)) // accept()
@@ -135,7 +166,7 @@ int main(int argc, char **argv)
             addrlen = sizeof(client_addr);
             client_fd = accept(listen_fd,
                                (struct sockaddr *)&client_addr, &addrlen);
-	        nonblock(client_fd);
+            nonblock(client_fd);
             FD_SET(client_fd, &readfds);
 
             if (client_fd > maxfd)
@@ -149,30 +180,127 @@ int main(int argc, char **argv)
             sockfd = i;
             if (FD_ISSET(sockfd, &allfds))
             {
-                if ((readn = read(sockfd, buf, MAXLINE - 1)) == 0)
+
+                if ((readn = read(sockfd, buf, sizeof(buf) - 1)) > 0)
                 {
-                    printf("close\n");
-                    close(sockfd);
-                    FD_CLR(sockfd, &readfds);
+                    memset(&req, 0, sizeof(req));
+                    memset(aBuffer, 0, sizeof(aBuffer));
+                    memcpy(aBuffer, buf, 4);
+                    req.opcode = atoi(aBuffer);
+                    memcpy(aBuffer, &buf[4], 4);
+                    req.length = atoi(aBuffer);
+                    
+                    memcpy(req.data, &buf[8], req.length); //req.length);
+
+                    switch (req.opcode)
+                    {
+
+                    case 0x0: // Initial Connecting Information
+                        /*
+                            Data - 30Byte
+                            Opcode(4) Length(4) Data( Nick(30) ) EOF
+                        */
+                       {
+                        int login_success = 0;
+
+                        int nick_length = 0, pw_length = 0;
+                        memset(aBuffer, 0, sizeof(aBuffer));
+                        memcpy(aBuffer, &buf[8], 4);
+                        nick_length = atoi(aBuffer);
+                        memset(aBuffer, 0, sizeof(aBuffer));
+                        memcpy(aBuffer, &buf[12+nick_length], 4);
+                        pw_length = atoi(aBuffer); 
+
+                        char nickname[30];
+                        memset(nickname, 0, sizeof(nickname));
+                        memcpy(nickname, &buf[12], nick_length); 
+
+                        char password[30];
+                        memset(password, 0, sizeof(password));
+                        memcpy(password, &buf[16+nick_length], nick_length);
+                        printf("%s %s\n", nickname, password);
+                        for (int i = 0; i < 7; i++) {
+                            if (!strcmp(nickname, loginArr[i].nickname) && !strcmp(password, loginArr[i].password)) {
+                                login_success = 1;
+                                break;
+                            }
+                        }
+
+                        if (nick_length > 30)
+                        {
+                            perror("Nickname cannot be longer than 30 letters");
+                        }
+                        if (pw_length > 30)
+                        {
+                            perror("Password cannot be longer than 30 letters\n");
+                        }
+                        if (login_success == 0)
+                        {
+<<<<<<< Updated upstream
+                            memset(&u_data[sockfd], 0, sizeof(userdata));
+                            memcpy(&u_data[sockfd].nick, req.data, MAX_NICKNAME_LENGTH);
+                            u_data[sockfd].connected = 1;
+                        }
+
+                        printonlineusers(u_data);
+
+=======
+                            perror("Nickname and pw does not match");
+                            char err_msg[50];
+                            memset(err_msg, 0, sizeof(err_msg));
+                            strcpy(err_msg, "Nickname and pw does not match");
+                            write(sockfd, err_msg, strlen(err_msg));
+                            //--------
+                        }
+                        else if (login_success)
+                        {
+                            if ((on = fetchUser(u_data, nickname)) > -1)
+                            {
+                                sock_u_map[sockfd] = on;
+                                u_data[sock_u_map[sockfd]].sockfd = sockfd;
+                                u_data[sock_u_map[sockfd]].connected = 2;
+                            }
+                            else
+                            {
+                                sock_u_map[sockfd] = client;
+                                memset(&u_data[client], 0, sizeof(userdata));
+                                memcpy(&u_data[client].nick, nickname, MAX_NICKNAME_LENGTH);
+                                u_data[client].connected = 2;
+                                u_data[client++].sockfd = sockfd;
+                            }
+                            broadcastUserMsg(u_data);
+                        }
+
+                        printf("[%d] %s Connected.\n", u_data[sock_u_map[sockfd]].sockfd, u_data[sock_u_map[sockfd]].nick);
+                        //printonlineusers(u_data);
+                       }
+>>>>>>> Stashed changes
+                        break;
+
+                    case 0x1: // Message Send Request
+                        /*
+                            Data - ~ 1028 byte
+                            Opcode(4) Length(4) Data( Desc(4) Data(1024) )
+                        */
+                        break;
+
+                    default:
+                        break;
+                    }
+
+                    buf[readn] = 0;
+                    printf("[%d] = %s", sockfd, req.data);
+                    write(sockfd, buf, strlen(buf));
                 }
                 else
                 {
+                    printf("close\n");
+                    u_data[sockfd].connected = 0;
+                    close(sockfd);
+                    FD_CLR(sockfd, &readfds);
+                    printonlineusers(u_data);
+                }
 
-                    do
-                    {
-                        buf[readn] = 0;
-			            printf("[%d] = %s", sockfd, buf); 
-                        write(sockfd, buf, strlen(buf));
-                    } while((readn = read(sockfd, buf, sizeof(buf) - 1)) > 0);
-                
-		    if(readn == -1){
-			if(errno != EAGAIN)
-			{
-				close(sockfd);
-				FD_CLR(sockfd, &readfds);
-			}
-		    }
-		}
                 if (--fd_num <= 0)
                     break;
             }
@@ -182,22 +310,34 @@ int main(int argc, char **argv)
 
 void nonblock(int sockfd) // Making Socket non-blocking socket
 {
-	int opts;
-	opts = fcntl(sockfd, F_GETFL);
-	if(opts < 0)
-	{
-		printf("fctntl(F_GETFL) error\n");
-		exit(1);
-	}
-	opts = (opts | O_NONBLOCK);
-	if(fcntl(sockfd, F_SETFL, opts) < 0)
-	{
-		printf("fcntl(F_SETFL) error\n");
-		exit(1);
-	}
-
+    int opts;
+    opts = fcntl(sockfd, F_GETFL);
+    if (opts < 0)
+    {
+        printf("fctntl(F_GETFL) error\n");
+        exit(1);
+    }
+    opts = (opts | O_NONBLOCK);
+    if (fcntl(sockfd, F_SETFL, opts) < 0)
+    {
+        printf("fcntl(F_SETFL) error\n");
+        exit(1);
+    }
 }
 
-void itoa(int i, char *st) {
-        sprintf(st, "%d", i);
+void printonlineusers(userdata *arr)
+{
+    printf("\nCurrently Online:\n");
+    for (int i = 0; i < MAX_CLIENT; i++)
+    {
+        if (arr[i].connected == 1)
+        {
+            printf("[%d] %s\n", arr[i].sockfd, arr[i].nick);
+        }
+    }
+}
+
+void itoa(int i, char *st)
+{
+    sprintf(st, "%d", i);
 }
